ab2ceec69893b1b8ae2b71ced767636de7e18847
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2025-06-09 23:43:44 +0200

    example

--- usql-quick/examples/test.js
+++ usql-quick/examples/test.js
@@ -7,7 +7,7 @@ const pool = await Pool.open({
 const conn = await pool.get();
 
 await conn.transaction(async (conn) => {
-  await conn.exec("CREATE TABLE users (name TEST)");
+  await conn.exec("CREATE TABLE users (name TEXT)");
   await conn.exec("INSERT INTO users VALUES (?)", ["Rasmus"]);
 });
 
@@ -16,3 +16,10 @@ const rows = await conn.query("SELECT * FROM users");
 for (const row of rows) {
   console.log(JSON.stringify(row));
 }
+
+const schema = await conn.query("PRAGMA table_info(users)");
+
+for (const row of schema) {
+  console.log(JSON.stringify(row));
+}
+

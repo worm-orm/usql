408aa23f8dac06147fb3110aa167230fe72afd8b
Rasmus Kildevaeld <rasmuskildevaeld@gmail.com>
2025-09-02 22:51:36 +0200

    Export streams

--- usql/src/lib.rs
+++ usql/src/lib.rs
@@ -20,8 +20,16 @@ pub use usql_util as util;
 pub use usql_value as value;
 
 pub use self::{
-    conn::Conn, error::Error, from_row::FromRow, pool::Pool, query::IntoQuery, row::Row,
-    stream::QueryStream, target::Target, trans::Trans, typed::Typed,
+    conn::Conn,
+    error::Error,
+    from_row::FromRow,
+    pool::Pool,
+    query::IntoQuery,
+    row::Row,
+    stream::{FromRowStream, ProjectStream, QueryStream},
+    target::Target,
+    trans::Trans,
+    typed::Typed,
 };
 
 #[cfg(feature = "derive")]

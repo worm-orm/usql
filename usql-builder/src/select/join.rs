eede3209f5f6eb3a3a176290822d9ef89c70ca30
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2025-09-06 21:14:37 +0200

    Revert

--- usql-builder/src/select/join.rs
+++ usql-builder/src/select/join.rs
@@ -146,7 +146,7 @@ impl JoinType {
     }
 }
 
-pub type BoxJoinable<'val> = Box<dyn DynJoinable<'val> + 'val>;
+pub type BoxJoinable<'val> = Box<dyn DynJoinable<'val>>;
 pub trait DynJoinable<'val> {
     fn build(self: Box<Self>, ctx: &mut Context<'val>) -> Result<(), Error>;
 }
@@ -174,9 +174,9 @@ where
     }
 }
 
-pub fn joinable_box<'val, T>(joinable: T) -> Box<dyn DynJoinable<'val> + 'val>
+pub fn joinable_box<'val, T>(joinable: T) -> Box<dyn DynJoinable<'val>>
 where
-    T: Joinable<'val> + 'val,
+    T: Joinable<'val> + 'static,
 {
     Box::new(JoinableBox(joinable))
 }
